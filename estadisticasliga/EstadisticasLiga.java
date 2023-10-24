cb511d4cf08ad43f019ddfbc0e3410c2e3e03229
ilopez000 <ignacio.lopez.aylagas@gmail.com>
2023-10-24 18:29:05 +0200

    Update EstadisticasLiga.java

--- estadisticasliga/EstadisticasLiga.java
+++ estadisticasliga/EstadisticasLiga.java
@@ -12,12 +12,20 @@ public class EstadisticasLiga {
 
     public static void main(String[] args) {
         Temporada temporada=new Temporada("2022", "C:\\Users\\ignac\\Documents\\NetBeansProjects\\EstadisticasLiga\\src\\main\\java\\com\\mycompany\\estadisticasliga\\SP1.csv");
-        temporada.imprimir_partidos();
-        System.out.println("Media de goles que mete el equipo local: "+
+        temporada.diccionario_partidos_ganados();
+        temporada.imprimir_diccionario();
+        //temporada.imprimir_partidos();
+        
+        /*System.out.println("Media de goles que mete el equipo local: "+
                 temporada.media_goles_equipos_locales());
         System.out.println("Media de goles que mete el Betis como local: "+
                 temporada.media_goles_equipos_locales("Betis"));
         System.out.println("Media de goles que mete el Betis como visitante: "+
                 temporada.media_goles_equipos_visitantes("Betis"));
+        */
+        
+        //System.out.println("Media de Goles que ha recibido el Betis: "+temporada.calcula_media_de_goles_recibidos("Betis"));
+
+        
     }
 }
